547df4a302e043b42a2dd0c5ad75fae313ce14d2
alomaryam <alomaryam@gmail.com>
2021-02-02 19:57:37 +0300

    Solved the Conditionals task

--- calculator.js
+++ calculator.js
@@ -1,3 +1,13 @@
-// Define the numbers and the operation here
+let firstValue = 5;
+let secondValue = 3;
+let operation = "/";
 
-// Write if-statements here
+if (operation === "+" ){
+    console.log(` equation: ${firstValue} + ${secondValue} = ${firstValue + secondValue}`);
+} else if (operation === "-"){
+   console.log(` equation: ${firstValue} - ${secondValue} = ${firstValue - secondValue}`);
+} else if (operation ==="*"){
+   console.log(` equation: ${firstValue} * ${secondValue} = ${firstValue * secondValue}`);
+} else if (operation === "/"){
+    console.log(` equation: ${firstValue} / ${secondValue} = ${firstValue / secondValue}`);
+};
